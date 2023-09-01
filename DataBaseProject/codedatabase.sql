ffe2f9e41e6ee75f87949e9545617715ff5009d5
Meawz <123901325+Meawz@users.noreply.github.com>
2023-09-01 18:17:04 +0300

    Update codedatabase.sql

--- DataBaseProject/codedatabase.sql
+++ DataBaseProject/codedatabase.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE login_db /* Creeaza baza de date */
+CREATE DATABASE login_db /* Create database */
 
 CREATE TABLE Users (
     ID int,
     Name varchar(255),
     Email varchar(255),
     Password_hash(255)
-) /* Creeaza tabelul cu date in db */
+) /* Create table inside the db */
 
-ALTER TABLE users MODIFY ID int NOT NULL PRIMARY KEY AUTO_INCREMENT /* Modifica tabelul, se pune auto incrementare pe coloana ID */
+ALTER TABLE users MODIFY ID int NOT NULL PRIMARY KEY AUTO_INCREMENT /* Modify table, auto increment on ID column */
 
-ALTER TABLE `users` ADD UNIQUE(`Email`) /* Modifica tabelul, se pune UNIQUE INDEX pe coloana Email */
+ALTER TABLE `users` ADD UNIQUE(`Email`) /*  Modify table, UNIQUE INDEX on Email column */
